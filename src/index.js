3171532c64156942fab90ce435c3426b8e4086bf
shevchenko-andrey <andreases_mobimoz@ukr.net>
2022-01-25 19:53:43 +0200

    fix code

--- src/index.js
+++ src/index.js
@@ -2,18 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SimpleReactLightbox from 'simple-react-lightbox';
 import App from 'components/App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
-
-function ErrorFallback({ error, resetErrorBoundary }) {
-  return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
-    </div>
-  );
-}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -23,8 +12,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
